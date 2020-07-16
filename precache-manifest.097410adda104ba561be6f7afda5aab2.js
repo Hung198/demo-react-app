0ba83b39bf241cc76a187b1708cbf6b6c269fb28
hung <hung.nguyen.son@nextop.asia>
2020-07-16 10:56:23 +0700

    Updates

---
+++ precache-manifest.097410adda104ba561be6f7afda5aab2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c508f1722f1d42672b52b38f3d984c7",
+    "url": "/demo-react-app/index.html"
+  },
+  {
+    "revision": "c2f18f7e360ae36476e8",
+    "url": "/demo-react-app/static/css/main.bcea9c19.chunk.css"
+  },
+  {
+    "revision": "f9d2678f6146115e2e65",
+    "url": "/demo-react-app/static/js/2.8ec6ff21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-react-app/static/js/2.8ec6ff21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2f18f7e360ae36476e8",
+    "url": "/demo-react-app/static/js/main.10442fcf.chunk.js"
+  },
+  {
+    "revision": "2b1ccb8fb9c18b594f65",
+    "url": "/demo-react-app/static/js/runtime-main.e67ffe46.js"
+  }
+]);
